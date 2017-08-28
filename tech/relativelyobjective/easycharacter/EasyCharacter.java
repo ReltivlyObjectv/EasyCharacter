e6e3ef0a4df05a41f0cdacfedf0fe4a4700141dc
ReltivlyObjectv <me@relativelyobjective.tech>
2017-08-27 17:58:15 -0700

    Created Repository

---
+++ tech/relativelyobjective/easycharacter/EasyCharacter.java
@@ -0,0 +1,12 @@
+package tech.relativelyobjective.easycharacter;
+
+/**
+ *
+ * @author ReltivlyObjectv
+ */
+public class EasyCharacter {
+	public static void main(String[] args) {
+		//TODO
+	}
+	
+}
