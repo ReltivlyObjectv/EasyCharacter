63ee8fdcacae60181143c6497e13ee69a9d5c8c7
ReltivlyObjectv <me@relativelyobjective.tech>
2017-08-30 20:36:20 -0700

    Added Khajiit setup

--- tech/relativelyobjective/easycharacter/races/Khajiit.java
+++ tech/relativelyobjective/easycharacter/races/Khajiit.java
@@ -1,6 +1,12 @@
 package tech.relativelyobjective.easycharacter.races;
 
+import tech.relativelyobjective.easycharacter.characterelements.AbilityModifier;
+import tech.relativelyobjective.easycharacter.characterelements.Darkvision;
+import tech.relativelyobjective.easycharacter.characterelements.Language;
+import tech.relativelyobjective.easycharacter.characterelements.SkillProficiency;
 import tech.relativelyobjective.easycharacter.utilities.InformationManager;
+import tech.relativelyobjective.easycharacter.utilities.Lists;
+import tech.relativelyobjective.easycharacter.utilities.WindowManager;
 
 /**
  *
@@ -8,6 +14,14 @@ import tech.relativelyobjective.easycharacter.utilities.InformationManager;
  */
 public class Khajiit {
 	public static void setup() {
-		System.out.println("//TODO -- Setup Race: "+InformationManager.selectedRace);
+		InformationManager.resetRaceElements();
+		InformationManager.addRaceElement(new SkillProficiency(Lists.Skill.STEALTH, 1));
+		InformationManager.addRaceElement(new SkillProficiency(Lists.Skill.SLEIGHT_OF_HAND, 1));
+		InformationManager.addRaceElement(new Language("Cyrodilic"));
+		InformationManager.addRaceElement(new Language("Ta'agra"));
+		InformationManager.addRaceElement(new AbilityModifier(Lists.Ability.DEXTERITY,2));
+		InformationManager.addRaceElement(new AbilityModifier(Lists.Ability.INTELLIGENCE,1));
+		InformationManager.addRaceElement(new Darkvision(120));
+		WindowManager.getRaceTab().updateRaceElementsList();
 	}
 }
