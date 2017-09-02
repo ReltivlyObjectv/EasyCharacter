a61179632f627a275d5f7e3a283029d9c9b810c4
ReltivlyObjectv <me@relativelyobjective.tech>
2017-09-02 12:51:55 -0700

    Added Half-Orc setup

--- tech/relativelyobjective/easycharacter/races/HalfOrc.java
+++ tech/relativelyobjective/easycharacter/races/HalfOrc.java
@@ -1,6 +1,15 @@
 package tech.relativelyobjective.easycharacter.races;
 
+import tech.relativelyobjective.easycharacter.characterelements.AbilityModifier;
+import tech.relativelyobjective.easycharacter.characterelements.Darkvision;
+import tech.relativelyobjective.easycharacter.characterelements.Feature;
+import tech.relativelyobjective.easycharacter.characterelements.Language;
+import tech.relativelyobjective.easycharacter.characterelements.Race;
+import tech.relativelyobjective.easycharacter.characterelements.SkillProficiency;
+import tech.relativelyobjective.easycharacter.characterelements.WalkSpeed;
 import tech.relativelyobjective.easycharacter.utilities.InformationManager;
+import static tech.relativelyobjective.easycharacter.utilities.Lists.Skill.*;
+import static tech.relativelyobjective.easycharacter.utilities.Lists.Ability.*;
 
 /**
  *
@@ -8,6 +17,26 @@ import tech.relativelyobjective.easycharacter.utilities.InformationManager;
  */
 public class HalfOrc {
 	public static void setup() {
-		System.out.println("//TODO -- Setup Race: "+InformationManager.selectedRace);
+		InformationManager.resetRaceElements();
+		InformationManager.addRaceElement(new Race("Half-Elf"));
+		InformationManager.addRaceElement(new AbilityModifier(STRENGTH,2));
+		InformationManager.addRaceElement(new AbilityModifier(CONSTITUTION,1));
+		InformationManager.addRaceElement(new WalkSpeed(30));
+		InformationManager.addRaceElement(new Darkvision(60));
+		InformationManager.addRaceElement(new SkillProficiency(INTIMIDATION,1));
+		InformationManager.addRaceElement(new Feature(
+			"Relentless Endurance",
+			"When you are reduced to 0 hit points but not killed outright, you "+
+			"can drop to 1 hit point instead. You can't use this feature again "+
+			"until you finish a long rest."
+		));
+		InformationManager.addRaceElement(new Feature(
+			"Savage Attacks",
+			"When you score a critical hit with a melee weapon attack, you can "+
+			"roll one of the weapon's damage dice one additional time and add "+
+			"it to the extra damage of the critical hit."
+		));
+		InformationManager.addRaceElement(new Language("Common"));
+		InformationManager.addRaceElement(new Language("Orc"));
 	}
 }
