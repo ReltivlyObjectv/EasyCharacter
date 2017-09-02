c26f5e73e7163a5e292357f40fcce15a91333ae5
ReltivlyObjectv <me@relativelyobjective.tech>
2017-09-02 13:51:28 -0700

    Added Tiefling setup

--- tech/relativelyobjective/easycharacter/races/Tiefling.java
+++ tech/relativelyobjective/easycharacter/races/Tiefling.java
@@ -1,6 +1,16 @@
 package tech.relativelyobjective.easycharacter.races;
 
+import tech.relativelyobjective.easycharacter.characterelements.AbilityModifier;
+import tech.relativelyobjective.easycharacter.characterelements.DamageModifier;
+import tech.relativelyobjective.easycharacter.characterelements.Darkvision;
+import tech.relativelyobjective.easycharacter.characterelements.Feature;
+import tech.relativelyobjective.easycharacter.characterelements.Language;
+import tech.relativelyobjective.easycharacter.characterelements.Race;
+import tech.relativelyobjective.easycharacter.characterelements.Spell;
+import tech.relativelyobjective.easycharacter.characterelements.WalkSpeed;
 import tech.relativelyobjective.easycharacter.utilities.InformationManager;
+import tech.relativelyobjective.easycharacter.utilities.Lists;
+import tech.relativelyobjective.easycharacter.utilities.WindowManager;
 
 /**
  *
@@ -8,6 +18,25 @@ import tech.relativelyobjective.easycharacter.utilities.InformationManager;
  */
 public class Tiefling {
 	public static void setup() {
-		System.out.println("//TODO -- Setup Race: "+InformationManager.selectedRace);
+		InformationManager.resetRaceElements();
+		InformationManager.addRaceElement(new Race("Tiefling"));
+		InformationManager.addRaceElement(new AbilityModifier(Lists.Ability.CHARISMA,2));
+		InformationManager.addRaceElement(new AbilityModifier(Lists.Ability.INTELLIGENCE,1));
+		InformationManager.addRaceElement(new WalkSpeed(30));
+		InformationManager.addRaceElement(new Darkvision(60));
+		InformationManager.addRaceElement(new DamageModifier(
+			"Fire", Lists.DamageMod.RESISTANT)
+		);
+		InformationManager.addRaceElement(new Spell("Thaumaturgy",0));
+		InformationManager.addRaceElement(new Feature(
+			"Infernal Legacy",
+			"When you reach 3rd level, you can cast the hellish rebuke "+
+			"spell once per day as a 2nd-level spell. Once you reach 5th level, "+
+			"you can cast the darkness spell once per day. Charisma is your "+
+			"spellcasting abilily for these spells."
+		));
+		InformationManager.addRaceElement(new Language("Common"));
+		InformationManager.addRaceElement(new Language("Infernal"));
+		WindowManager.getRaceTab().updateRaceElementsList();
 	}
 }
