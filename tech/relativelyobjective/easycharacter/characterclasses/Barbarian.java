093dc5b8845541375257fa059bbed77678f2da25
ReltivlyObjectv <me@relativelyobjective.tech>
2017-09-09 14:48:10 -0700

    Added Barbarian Skill prompt

--- tech/relativelyobjective/easycharacter/characterclasses/Barbarian.java
+++ tech/relativelyobjective/easycharacter/characterclasses/Barbarian.java
@@ -1,9 +1,21 @@
 package tech.relativelyobjective.easycharacter.characterclasses;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import static javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE;
 import tech.relativelyobjective.easycharacter.characterelements.InventoryItem;
 import tech.relativelyobjective.easycharacter.characterelements.OtherProficiency;
 import tech.relativelyobjective.easycharacter.characterelements.ProficiencyBonus;
 import tech.relativelyobjective.easycharacter.characterelements.SavingThrowProficiency;
+import tech.relativelyobjective.easycharacter.characterelements.SkillProficiency;
 import tech.relativelyobjective.easycharacter.utilities.InformationManager;
 import tech.relativelyobjective.easycharacter.utilities.ItemLists;
 import tech.relativelyobjective.easycharacter.utilities.Lists;
@@ -14,6 +26,14 @@ import tech.relativelyobjective.easycharacter.utilities.WindowManager;
  * @author ReltivlyObjectv
  */
 public class Barbarian {
+	public static Lists.Skill[] barbarianSkills = {
+		Lists.Skill.ANIMAL_HANDLING,
+		Lists.Skill.ATHLETICS,
+		Lists.Skill.INTIMIDATION,
+		Lists.Skill.NATURE,
+		Lists.Skill.PERCEPTION,
+		Lists.Skill.SURVIVAL
+	};
 	public static void setup(int level) {
 		InformationManager.addClassElement(
 			new ProficiencyBonus(ClassChoices.getProficiencyBonus(level))
@@ -100,7 +120,53 @@ public class Barbarian {
 		WindowManager.getClassTab().updateClassElementsList();
 	}
 	private static void showLevelOneSkillProficiencyPrompt() {
-		//Choose proficiency in two: Animal Handle, Athletics, Intim, Nature, Perception, Survival
+		//Gain proficiency in two skills
+		JDialog prompt = new JDialog(WindowManager.getMainFrame(), "Barbarian Skills", true);
+		prompt.setPreferredSize(new Dimension(450,500));
+		prompt.setSize(prompt.getPreferredSize());
+		prompt.setMaximumSize(prompt.getPreferredSize());
+		prompt.setMinimumSize(prompt.getPreferredSize());
+		prompt.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		prompt.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		JLabel header = new JLabel(
+			"<html><strong>Choose two different skills to gain proficiency in</strong></html>", 
+			JLabel.CENTER);
+		prompt.add(header, constraints);
+		constraints.gridy++;
+		HashMap<Lists.Skill, JCheckBox> checkBoxes = new HashMap<>();
+		for (int i = 0; i < barbarianSkills.length; i++) {
+			JCheckBox box = new JCheckBox(Lists.SKILLS[Lists.getSkillIndex(barbarianSkills[i])]);
+			checkBoxes.put(barbarianSkills[i], box);
+			prompt.add(box, constraints);
+			constraints.gridy++;
+		}
+		JButton saveButton = new JButton("Add Proficiencies");
+		saveButton.addActionListener((ActionEvent e)->{
+			int checkedBoxes = 0;
+			for (Lists.Skill s : barbarianSkills) {
+				if (checkBoxes.get(s).isSelected()) {
+					checkedBoxes++;
+				}
+			}
+			if (checkedBoxes == 2) {
+				//Correct
+				for (Lists.Skill s : barbarianSkills) {
+					if (checkBoxes.get(s).isSelected()) {
+						InformationManager.addClassElement(new SkillProficiency(s,1));
+					}
+				}
+				WindowManager.getClassTab().updateClassElementsList();
+				prompt.dispose();
+			} else {
+				//Incorrect
+				JOptionPane.showMessageDialog(prompt, "Select exactly two skills.");
+			}
+		});
+		prompt.add(saveButton, constraints);
+		prompt.setVisible(true);
 	}
 	private static void showStartingEquipmentPrompt() {
 		//phb p48
