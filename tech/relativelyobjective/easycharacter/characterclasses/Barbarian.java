bac419458347a9518b95e5a3992a3053b6df309a
ReltivlyObjectv <me@relativelyobjective.tech>
2017-09-22 16:26:37 -0700

    Added Barbarian level 2

--- tech/relativelyobjective/easycharacter/characterclasses/Barbarian.java
+++ tech/relativelyobjective/easycharacter/characterclasses/Barbarian.java
@@ -75,11 +75,27 @@ public class Barbarian {
 				"10 + your Dexterity modifier + your Constitution modifier. "+
 				"You can use a shield and gain this benefit."
 			));
-			
 			WindowManager.getClassTab().updateClassElementsList();
 			showLevelOneSkillProficiencyPrompt();
 		}
 		if (level >= 2) {
+			InformationManager.addClassElement(new Feature(
+				"Reckless Attack",
+				"You can throw aside all concern for defense to attack with "+
+				"fierce desperation. When you make your first attack on your "+
+				"turn, you can decide to attack recklessly. Doing so gives you "+
+				"advantage on melee weapon attack rolls using Strength during "+
+				"this turn, but attack rolls against you have advantage until "+
+				"your next turn."
+			));
+			InformationManager.addClassElement(new Feature(
+				"Danger Sense",
+				"You gain an uncanny sense of when things nearby aren't as they "+
+				"should be, giving you an edge when you dodge away from danger. "+
+				"You have advantage on Dexterity saving throws against effects "+
+				"that you can see, such as traps and spells. To gain this benefit, "+
+				"you can't be blinded, deafened, or incapacitated."
+			));
 			
 		}
 		if (level >= 3) {
