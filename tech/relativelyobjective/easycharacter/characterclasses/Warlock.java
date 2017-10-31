21bae68febd87b7789b796e7e0b2b403357e9395
ReltivlyObjectv <me@relativelyobjective.tech>
2017-10-30 19:32:53 -0700

    Added skeleton if statements to all classes

--- tech/relativelyobjective/easycharacter/characterclasses/Warlock.java
+++ tech/relativelyobjective/easycharacter/characterclasses/Warlock.java
@@ -6,6 +6,46 @@ package tech.relativelyobjective.easycharacter.characterclasses;
  */
 public class Warlock {
 	public static void setup(int level) {
+		if (level >= 1) {
+		}
+		if (level >= 2) {
+		}
+		if (level >= 3) {
+		}
+		if (level >= 4) {
+		}
+		if (level >= 5) {
+		}
+		if (level >= 6) {
+		}
+		if (level >= 7) {
+		}
+		if (level >= 8) {
+		}
+		if (level >= 9) {
+		}
+		if (level >= 10) {
+		}
+		if (level >= 11) {
+		}
+		if (level >= 12) {
+		}
+		if (level >= 13) {
+		}
+		if (level >= 14) {
+		}
+		if (level >= 15) {
+		}
+		if (level >= 16) {
+		}
+		if (level >= 17) {
+		}
+		if (level >= 18) {
+		}
+		if (level >= 19) {
+		}
+		if (level >= 20) {
+		}
 		System.out.println("//TODO Setup Class: "+Warlock.class);
 	}
 }
