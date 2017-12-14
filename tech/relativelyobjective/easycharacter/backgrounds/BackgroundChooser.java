37ab92a464da6e10c998db0440d675a31f30ad1c
ReltivlyObjectv <me@relativelyobjective.tech>
2017-12-14 04:19:34 -0800

    Implemented Sage background

--- tech/relativelyobjective/easycharacter/backgrounds/BackgroundChooser.java
+++ tech/relativelyobjective/easycharacter/backgrounds/BackgroundChooser.java
@@ -365,7 +365,39 @@ public class BackgroundChooser {
 				WindowManager.getBackgroundTab().updateBackgroundElementsList();
 				break;
 			case SAGE:
-				//TODO
+				InformationManager.addBackgroundElement(new SkillProficiency(Lists.Skill.ARCANA));
+				InformationManager.addBackgroundElement(new SkillProficiency(Lists.Skill.HISTORY));
+				WindowManager.getBackgroundTab().updateBackgroundElementsList();
+				String[] sageLanguages = MiscPrompts.openMultipleObjectChooserPrompt(
+					InformationManager.getUnknownLanguages(),
+					"Additional Languages",
+					2,
+					String.class
+				);
+				for (String lang : sageLanguages) {
+					InformationManager.addBackgroundElement(new Language(lang));
+				}
+				InformationManager.addBackgroundElement(new InventoryItem("Bottle of Black Ink"));
+				InformationManager.addBackgroundElement(new InventoryItem("Quill"));
+				InformationManager.addBackgroundElement(new InventoryItem("Small Knife"));
+				InformationManager.addBackgroundElement(new InventoryItem(
+					"A Letter from a Dead Colleague Posing a Question You Have Not Yet Been Able to Answer,"
+				));
+				InformationManager.addBackgroundElement(new InventoryItem("Common Clothes"));
+				InformationManager.addBackgroundElement(new InventoryItem("Gold", 10));
+				InformationManager.addBackgroundElement(new Feature(
+					"Researcher",
+					"When you attempt to learn or recall a piece of lore, "+
+					"if you do not know that information, you often know where "+
+					"and from whom you can obtain it. Usually, this information "+
+					"comes from a library, scriptorium, university, or a sage "+
+					"or other learned person or creature. Your DM might rule "+
+					"that the knowledge you seek is secreted away in an almost "+
+					"inaccessible place, or that it simply cannot be found. "+
+					"Unearthing the deepest secrets of the multiverse can "+
+					"require an adventure or even a whole campaign."
+				));
+				WindowManager.getBackgroundTab().updateBackgroundElementsList();
 				break;
 			case SAILOR:
 				//TODO
