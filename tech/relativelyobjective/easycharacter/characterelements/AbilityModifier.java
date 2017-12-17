684895d1d9664d0f1cf47616008e1c60bc613912
ReltivlyObjectv <me@relativelyobjective.tech>
2017-12-17 15:41:24 -0800

    Removed unused imports from AbilityModifier

--- tech/relativelyobjective/easycharacter/characterelements/AbilityModifier.java
+++ tech/relativelyobjective/easycharacter/characterelements/AbilityModifier.java
@@ -1,23 +1,7 @@
 package tech.relativelyobjective.easycharacter.characterelements;
 
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.util.HashMap;
-import java.util.TreeSet;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
-import static javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE;
-import javax.swing.event.ChangeEvent;
 import tech.relativelyobjective.easycharacter.utilities.InformationManager;
 import tech.relativelyobjective.easycharacter.utilities.Lists;
-import tech.relativelyobjective.easycharacter.utilities.Lists.Ability;
-import tech.relativelyobjective.easycharacter.utilities.WindowManager;
 
 /**
  *
