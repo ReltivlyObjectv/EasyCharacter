0c3b9edf740887b96e8c5fa90902fdbf80d9dfe8
ReltivlyObjectv <me@relativelyobjective.tech>
2017-12-17 15:37:41 -0800

    Improved Feat class
    It now has a description and supports holding ability score improvements and other character elements

--- tech/relativelyobjective/easycharacter/characterelements/Feat.java
+++ tech/relativelyobjective/easycharacter/characterelements/Feat.java
@@ -6,11 +6,36 @@ package tech.relativelyobjective.easycharacter.characterelements;
  */
 public class Feat implements CharacterElement,Comparable {
 	public String name;
+	public String description;
+	private final CharacterElementList effects;
+	
+	public Feat() {
+		name = "New Feat";
+		description = "";
+		effects = new CharacterElementList();
+	}
 	public Feat(String newName) {
 		name = newName;
+		description = "";
+		effects = new CharacterElementList();
+	}
+	public Feat(String newName, String newDesc) {
+		name = newName;
+		description = newDesc;
+		effects = new CharacterElementList();
+	}
+	public void addFeatElement(CharacterElement e) {
+		if (e instanceof Feat) {
+			throw new UnsupportedOperationException("You cannot store a Feat inside of a Feat");
+		}
+		effects.addCharacterElement(e);
+	}
+	public void resetFeatElements() {
+		effects.resetCharacterElements();
 	}
 	@Override
 	public void edit(boolean viewOnly) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 	}
 	@Override
 	public String toString() {
